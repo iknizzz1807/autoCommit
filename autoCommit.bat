d452e161ebec462c553cdc36e684b8fd2fa5b6bd
Nguyen My Thong <90298623+iknizzz1807@users.noreply.github.com>
2024-07-18 23:08:52 +0700

    Add more stuffs

---
+++ autoCommit.bat
@@ -0,0 +1,25 @@
+@echo off
+
+color a
+
+D:
+
+cd coding
+
+REM Clone repository (replace git@github.com:username/repo.git with your actual repo URL)
+git clone git@github.com:iknizzz1807/autoCommit.git
+
+REM Navigate to repository directory
+cd autoCommit
+
+REM Make changes (replace with actual commands)
+python -u "D:\Coding\Python\generateText.py"
+
+REM Stage changes
+git add .
+
+REM Commit changes with a default message
+git commit -m "Auto commit"
+
+REM Push changes
+git push
